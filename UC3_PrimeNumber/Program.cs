997d3eb3c6abd24b929e989d6f6aab3423991b11
YogitaDevi <yogita.gry@gmail.com>
2022-12-01 18:09:14 +0530

    PrimeNumber

---
+++ UC3_PrimeNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UC3_PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number : ");
+            int n = int.Parse(Console.ReadLine());
+            int a = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    a++;
+                }
+            }
+            if (a == 2)
+            {
+                Console.WriteLine("{0} is a Prime Number", n);
+            }
+            else
+            {
+                Console.WriteLine("Not a Prime Number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
